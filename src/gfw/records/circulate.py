fcad2ee9ecde8b41f7f3c79e688dac59796398fb
nKsnC <1808025988@qq.com>
2020-02-17 17:23:11 +0800

    【更新】记录处理程序

--- src/gfw/records/circulate.py
+++ src/gfw/records/circulate.py
@@ -16,7 +16,8 @@ import time
 
 class CirculateRecord(object):
     """
-    流通记录。
+    流通记录类，是用来处理流通记录的。
+
     记录：版本+流通内容+认证内容；此类是流通内容部分。
     流通内容：商品ID，时间戳，序号，流通标识。
         商品ID：唯一标识商品的一串数字。
@@ -24,18 +25,54 @@ class CirculateRecord(object):
         序号：此商品ID流通的序号。（用于标识流通环节）
         流通标识：4bit，用于标识流通环节的具体内容。
     """
-    def __init__(self,id,circulate_flag):
-        """
-        :param id: 商品id
-        :param circulate_flag: 流通标识（取值）
-            生产：生产到销售：销售中：被购买：快递中：消费者手中：
-        """
-        self.id=id
-        self.circulate_flag=circulate_flag
+    def __init__(self, id, version):
+        self.id = id #商品ID
+        self.version = version
 
-    def new_circulate_record(self，seq = 0):
+    def new_circulate_record(self, id, circulate_flag, pre_crec):
         """
         创建一条流通记录。
+
+        :param id: 商品id
+        :param circulate_flag: 流通标识（取值）
+            生产(0000)：生产到销售(0001)：销售中(0010)：被购买(0011)：快递中(0100)：消费者手中(0101)：
+        :param pre_rec:此商品ID之前的记录，list类型，记录是dict类型。
         """
-        self.time=time.time()
-        self.seq=seq # TODO(ZHOU) 序号如何生成，序号代表；这得从头遍历，找到此ID之前有没有，才能生成这个序号啊
+        flag=self.valid_crec(pre_crec)
+        new = dict()
+
+        if flag is False:
+            raise ValueError('商品流通值有误!')
+        elif circulate_flag==b'0000':
+            new['id']=id
+            new['circulate_flag']=circulate_flag
+            new['seq']=0
+            new['time']=int(time.time())
+        else:
+            new['id']=id
+            new['circulate_flag']=circulate_flag
+            new['seq']=sorted(pre_crec,key=dict.get('seq'))[-1]['seq']+1 # 对列表中字典进行排序,选择最后一个seq+1；bug如果之前没有记录会出错，不过那是特使的记录，在此不表
+            new['time']=int(time.time())
+
+        return new
+
+    # v0 版本规则验证
+    def valid_crec(self,pre_crec):
+        """
+        GFW模型规则检查。
+        v0版本：只有三个标识，顺序不变
+
+        :param pre_crec: 之前的流通记录,包含多个dict的列表。
+        """
+
+        # 假设pre_crec为包含多个crec的列表
+        for d in pre_crec:
+            if d['circulate_flag']==b'0000' and d['seq']==b'0':
+                continue
+            elif d['circulate_flag']==b'0001' and d['seq']==b'1':
+                continue
+            elif d['circulate_flag']==b'0011' and d['seq']==b'2':
+                continue
+
+            # 不满足规则，这条记录就不符合规范
+            return False
--- src/gfw/records/record.py
+++ src/gfw/records/record.py
@@ -13,10 +13,9 @@
 import hashlib
 import json
 from fastecdsa import ecdsa
-from records import circulate
+from gfw.records.circulate import CirculateRecord
 
-
-version=0x # 流通规则版本
+version=0x0000 # 流通规则版本
 
 class Record(object):
     """
@@ -26,16 +25,36 @@ class Record(object):
     签名，公钥，只要能识别是谁发送就行了
     """
 
-    def __init__(self, pri_key, pub_key):
+    def __init__(self, id, pri_key, pub_key):
         """
+        :param id:商品id
         :param pri_key: 参与者私钥
         :param pub_key: 参与者公钥
         """
+        self.id=id
         self.pri_key=pri_key
         self.pub_key=pub_key
 
+    def new_record(self,circulate_flag):
+        """
+        生成一条记录。
+        版本，流通记录，验证脚本。
+        :param circulate_flag: 二进制数，流通标识
+        """
+
+        # TODO(ZHOU) 此商品ID之前所有的流通记录,先搜索，判断
+        id_pre_crecord=list()
+
+
+        self.new['version']=version
+        self.new['crec']=CirculateRecord.new_circulate_record(self.id,circulate_flag,id_pre_crecord)
+        self.new['pub_key']=self.pub_key
+        self.new['sign']=self.sign(self.pri_key,self.new)
+
+        return self.new
+ 
     # 签名验证机制
-     def sign(self, priv_key, record):
+    def sign(self, priv_key, record):
         """
         签名记录
         :param priv_key: 私钥
@@ -43,9 +62,26 @@ class Record(object):
         :return:
         """
 
-        sign_data=self.serialize()
+        sign_data=self.serialize(record)
         sign=ecdsa.sign(sign_data,priv_key) 
-        self.sign=sign
+        
+        return sign
+
+
+    def serialize(self, record):
+        """
+        #序列化记录。
+        # :return: dict类型
+        """
+        k=dict()
+        k['version']=record['version']
+        k['id']=record['crec']['id']
+        k['seq']=record['crec']['seq']
+        k['circulate_flag']=record['crec']['circulate_flag']
+        k['time']=record['crec']['time']
+        k['pub_key']=record['pub_key']
+
+        return k
 
     def vertify(self,record):
         """
@@ -54,43 +90,3 @@ class Record(object):
         """
         sign_data=self.serialize() # 这个不应该这样些，但就这样了，之后再完善
         ecdsa.verify(self.sign,sign_data,self.pub_key)
-
-    def serialize(self):
-        """
-        序列化记录。
-        :return: json类型
-        """
-        k=dict()
-        k['version']=self.version
-        k['id']=self.crec.id
-        k['circulate_flag']=self.crec.circulate_flag
-        k['timestamp']=self.crec.time
-        k['pub_key']=self.pub_key
-
-
-    # TODO(ZHOU) 返回一条完整的记录
-    def get_record(self):
-        pass
-
-    def new_record(self):
-        """
-        生成一条记录。
-        版本，流通记录，验证脚本。
-        """
-        self.version=version
-
-        # circulate.CirculateRecord
-
-# 测试
-if __name__=='__main__':
-    # TODO(ZHOU) 生成一条记录
-
-
-# TODO(ZHOU) 将商品ID的记录都找出来(之后在建立这个基础上，有一系列功能)
-
-# TODO(ZHOU) 记录各字段大小
-
-# TODO(ZHOU) 重构记录生成器
-"""
-gfw规则验证，
-"""
