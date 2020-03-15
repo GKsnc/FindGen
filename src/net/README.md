dde5a41310cb1fddad8f3abd4298e7efe45b9861
nKsnC <1808025988@qq.com>
2020-03-15 08:07:49 +0800

    【添加】p2p；未开发

---
+++ src/net/README.md
@@ -0,0 +1,7 @@
+# 使用libp2p
+
+![项目地址](https://github.com/libp2p/py-libp2p)
+
+注意：这个P2P库项目正在开发。
+
+不过没有找到其他用python实现的大型p2p项目。
---
+++ src/net/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+"""
+区块链网络通信部分。
+
+@File    :   __init__.py
+@Time    :   2020/03/15 07:43:48
+"""
